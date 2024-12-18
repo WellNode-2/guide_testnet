8f57421a3886762ebcdae78545c013a613a4d4a9
WellNode-2 <yarnik.node+2@gmail.com>
2024-12-18 19:15:11 +0200

    Update sharderium-monitoring.sh
    new version

--- Sharderium/sharderium-monitoring.sh
+++ Sharderium/sharderium-monitoring.sh
@@ -16,15 +16,15 @@ while true; do
     echo "$(date)"
 
     # Перевіряємо, чи працює контейнер
-    CONTAINER_STATUS=$(docker inspect -f '{{.State.Running}}' shardeum-dashboard)
+    CONTAINER_STATUS=$(docker inspect -f '{{.State.Running}}' shardeum-validator)
     if [[ "$CONTAINER_STATUS" != "true" ]]; then
-        send_telegram_message "Контейнер shardeum-dashboard не запущений. Спроба запуску..."
-        docker start shardeum-dashboard
+        send_telegram_message "Контейнер shardeum-validator не запущений. Спроба запуску..."
+        docker start shardeum-validator
         sleep 10
     fi
 
     # Отримуємо саме поле state
-    STATE=$(docker exec -t shardeum-dashboard operator-cli status | awk '/state:/ {print $NF}' | tr -d '[:space:]')
+    STATE=$(docker exec -t shardeum-validator operator-cli status | awk '/state:/ {print $NF}' | tr -d '[:space:]')
     echo "Отриманий статус: $STATE"
 
     if [[ -z "$STATE" || "$STATE" == "stopped" ]]; then
@@ -32,7 +32,7 @@ while true; do
             send_telegram_message "Нода все ще в стані 'stopped' після спроби перезапуску."
         fi
         echo "Спроба перезапустити ноду..."
-        docker exec -i shardeum-dashboard operator-cli start
+        docker exec -i shardeum-validator operator-cli start
         RESTART_STATUS=1
     elif [[ "$STATE" =~ ^(standby|waiting-for-network|active|ready|syncing|selected)$ ]]; then
         echo "$NAME Нода працює коректно."
